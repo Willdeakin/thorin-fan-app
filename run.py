24c2de81ce1119e874ef27ae3436e7c1216213b0
Willdeakin <willdeakin@outlook.com>
2021-06-24 13:20:33 +0000

    save before deploying to heroku

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,7 +19,8 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company = data)
+    return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -30,6 +32,7 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
@@ -37,14 +40,14 @@ def contact():
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
+
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
 
+
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP","0.0.0.0"),
-
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-        )
+        debug=True)
